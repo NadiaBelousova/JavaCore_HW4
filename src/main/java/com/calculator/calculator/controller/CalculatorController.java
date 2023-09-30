d9ec87fdd086e55599b45f6ab20c4e274d5c8743
Надежда <nadin94403@gmail.com>
2023-09-30 14:38:33 +0300

    дз по Spring1

---
+++ src/main/java/com/calculator/calculator/controller/CalculatorController.java
@@ -0,0 +1,39 @@
+package com.calculator.calculator.controller;
+
+import com.calculator.calculator.service.api.CalculatorServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorServiceImpl calculatorService;
+
+    public CalculatorController(CalculatorServiceImpl calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return calculatorService.hello();
+    }
+    @GetMapping (path="/plus")
+    public String addition (@RequestParam("num1") int num1,@RequestParam("num2") int num2)  {
+        return calculatorService.addition(num1,num2);
+    }
+    @GetMapping (path="/minus")
+    public String substraction (@RequestParam("num1") int num1,@RequestParam("num2") int num2)  {
+        return calculatorService.substraction(num1,num2);
+    }
+    @GetMapping (path="/multiply")
+    public String multiply (@RequestParam(required = true) int num1,@RequestParam(required = true) int num2)  {
+        return calculatorService.multiply(num1,num2);
+    }
+    @GetMapping (path="/divide")
+    public String divide (@RequestParam("num1") int num1,@RequestParam("num2") int num2)  {
+        return calculatorService.divide(num1,num2);
+    }
+
+}
---
+++ src/main/java/com/calculator/calculator/service/api/CalculatorServiceImpl.java
@@ -0,0 +1,35 @@
+package com.calculator.calculator.service.api;
+
+import com.calculator.calculator.service.impl.CalculatorService;
+import org.springframework.stereotype.Service;
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+
+    public String hello() {
+
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String addition (int num1, int num2)  {
+        int sum=num1+num2;
+        return  num1 + "+"+ num2+ "="+sum;
+    }
+    public String substraction (int num1, int num2)  {
+        int minus=num1-num2;
+        return  num1 + "-"+ num2+ "="+minus;
+    }
+    public String multiply (int num1, int num2)  {
+        int multiply =num1*num2;
+        return  num1 + "*"+ num2+ "="+multiply ;
+    }
+    public String divide (int num1, int num2)  {
+        if (num2 != 0) {
+            int divide = num1 / num2;
+            return num1 + "/" + num2 + "=" + divide;
+        } else {
+            return "на ноль делить нельзя";
+        }
+
+}
+}
